feeae4f253f7e8bfee7d14cb032adff00d507c5c
WaOdeAndini <andiniii940@gmail.com>
2025-05-30 12:40:37 +0700

    first commit

---
+++ json/latihan1.php
@@ -0,0 +1,24 @@
+<!-- <?php -->
+
+$mahasiswa = [
+    [
+        "nama" => "Wa Ode Andini",
+        "nim" => "2217020064",
+        "email" => "andiniii940@gmail.com"
+    ],
+    [
+        "nama" => "Rafa Riyandra",
+        "nim" => "2222121277",
+        "email" => "rafaryd@gmail.com"
+    ],
+];
+
+// $dbh = new PDO('mysql:host=localhost; dbname=dblatihan', 'root', 
+// ''); 
+// $db = $dbh->prepare('SELECT * FROM mahasiswa'); 
+// $db->execute(); 
+// $mahasiswa = $db->fetchAll(PDO::FETCH_ASSOC);
+
+// $data = json_encode($mahasiswa);
+// echo $data;
+// ?>
