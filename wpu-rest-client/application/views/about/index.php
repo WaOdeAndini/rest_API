335f5ee131def8c342aec7c6890af55028491483
WaOdeAndini <andiniii940@gmail.com>
2025-06-03 05:20:11 +0700

    menambahkan rest server client

---
+++ wpu-rest-client/application/views/about/index.php
@@ -0,0 +1,86 @@
+  <!-- About -->
+<div class="container">
+  <div class="row mb-4">
+    <div class="col text-center">
+      <h2></h2>
+    </div>
+  </div>
+
+    <!-- About -->
+    <section class="about" id="about">
+      <div class="container">
+        <div class="row mb-4">
+          <div class="col text-center">
+            <h2>About</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-5 text-justify">
+            <p class="text-justify">
+            <p>Hallo, saya Andin dan saat ini saya sedang menjalani kehidupan sebagai mahasiswa di UIN Imam Bonjol Padang dengan jurusan Sistem Informasi Fakultas Sains dan Teknologi. Saya lahir di Timur Indonesia dan menetap di Barat Indonesia yaitu Padang Sumatera Barat. Saya lulus dari SMA Negeri 1 Silaut di Pesisir Selatan pada tahun 2022 dengan jurusan MIPA.
+            </p>
+          </div>
+          <div class="col-md-5 text-justify">
+          <p class="text-justify">
+            <p>Dulu saat sekolah saya juga aktif sebagai anggota OSIS, mengikuti Ekskul Pramuka, Rohis, dan Marching Band. Dan Saat ini saya aktif sebagai anggota Senat Mahasiswa di Fakultas Sains dan Teknologi. Beberapa hal yang saya sukai yaitu membaca buku, menonton film/anime, traveling, mendengarkan musik, dan bernyanyi.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  <!-- Contact -->
+    <section class="contact bg-light" id="contact">
+      <div class="container">
+        <div class="row pt-4 mb-4">
+          <div class="col text-center">
+            <h2>Contact</h2>
+          </div>
+        </div>
+
+        <div class="row justify-content-center">
+          <div class="col-lg-4">
+            <div class="card bg-primary text-white mb-4 text-center">
+              <div class="card-body">
+                <h5 class="card-title">Contact Me</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+            
+            <ul class="list-group mb-4">
+              <li class="list-group-item"><h3>Location</h3></li>
+              <li class="list-group-item">My Office</li>
+              <li class="list-group-item">Jl. Sungai Bangek No. 12 Kota Padang</li>
+              <li class="list-group-item">West Sumatera, Indonesia</li>
+            </ul>
+          </div>
+
+          <div class="col-lg-6">
+            
+            <form>
+              <div class="form-group">
+                <label for="nama">Nama</label>
+                <input type="text" class="form-control" id="nama">
+              </div>
+              <div class="form-group">
+                <label for="email">Email</label>
+                <input type="text" class="form-control" id="email">
+              </div>
+              <div class="form-group">
+                <label for="phone">Phone Number</label>
+                <input type="text" class="form-control" id="phone">
+              </div>
+              <div class="form-group">
+                <label for="message">Message</label>
+                <textarea class="form-control" id="message" rows="3"></textarea>
+              </div>
+              <div class="form-group">
+                <button type="button" class="btn btn-primary">Send Message</button>
+              </div>
+            </form>
+
+          </div>
+        </div>
+      </div>
+    </section>
+
+
---
+++ wpu-rest-client/application/views/home/index.php
@@ -0,0 +1,89 @@
+
+
+    <div class="jumbotron" id="home">
+      <div class="container">
+        <div class="text-center">
+          <img src="<?= base_url(); ?>assets/img/profile3.png" class="rounded-circle" img-thumbnail style="width: 250px; height: 250px; object-fit: cover; border: 5px solid #fff; box-shadow: 0 0 10px rgba(0,0,0,0.15);">
+          <h1 class="display-4">Wa Ode Andini</h1>
+          <h3 class="lead">Collage Student | Business Man | Youtuber</h3>
+        </div>
+      </div>
+    </div>
+
+    
+    <!-- Portfolio -->
+    <section class="portfolio" id="portfolio">
+      <div class="container">
+        <div class="row pt-4 mb-4">
+          <div class="col text-center">
+            <h2>Portfolio</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md mb-4">
+            <div class="card">
+              <img class="card-img-top" src="<?= base_url('assets/img/thumbs/1.png'); ?>" alt="Card image cap">
+              <div class="card-body text-center">
+                <h5 class="card-title">Json</h5>
+                <p class="card-text">Latihan menggunakan json.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md mb-4">
+            <div class="card">
+              <img class="card-img-top" src="<?= base_url('assets/img/thumbs/2.png'); ?>" alt="Card image cap">
+              <div class="card-body text-center">
+                <h5 class="card-title">Rest API</h5>
+                <p class="card-text">Menampilkan Data Menu Pizza dengan website sederhana.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md mb-4">
+            <div class="card">
+              <img class="card-img-top" src="<?= base_url('assets/img/thumbs/3.png'); ?>" alt="Card image cap">
+              <div class="card-body text-center">
+                <h5 class="card-title">Public API</h5>
+                <p class="card-text">Menggunakan OMdb API untuk menampilkan daftar film dengan website sederhana.</p>
+              </div>
+            </div>
+          </div>   
+        </div>
+
+        <div class="row">
+          <div class="col-md mb-4">
+            <div class="card">
+              <img class="card-img-top" src="<?= base_url('assets/img/thumbs/4.png'); ?>" alt="Card image cap">
+              <div class="card-body text-center">
+                <h5 class="card-title"> API Social Media</h5>
+                <p class="card-text">Menghubungkan Youtube dan Instagram menggunakan API untuk ditampilkan diwbsite Portfolio.</p>
+              </div>
+            </div>
+          </div> 
+          <div class="col-md mb-4">
+            <div class="card">
+              <img class="card-img-top" src="<?= base_url('assets/img/thumbs/5.png'); ?>" alt="Card image cap">
+              <div class="card-body text-center">
+                <h5 class="card-title">Portofolio</h5>
+                <p class="card-text">Menampilkan data diri sendiri dengan menggunakan API
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md mb-4">
+            <div class="card">
+              <img class="card-img-top" src="<?= base_url('assets/img/thumbs/6.png'); ?>" alt="Card image cap">
+              <div class="card-body text-center">
+                <h5 class="card-title">Rest Server</h5>
+                <p class="card-text">Latihan Membuat rest server menggunakan CodeIgniter.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
